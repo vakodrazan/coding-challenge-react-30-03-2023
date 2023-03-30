7a0e872d7023dbe9b44e208df3f7573eabe4d31d
noeline <noeline.mar@onja.org>
2023-03-30 19:57:57 +0300

    Adding an audio player

--- src/AudioPlay.js
+++ src/AudioPlay.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
-var a;
+var audioFile;
 const AudioPlay = () => {
   const [buttonName, setButtonName] = useState('Play');
 
   const [audio, setAudio] = useState();
+  const [audioInfo, setAudioInfo] = useState();
+
+  console.log('audio::::::', audio);
+  console.log('audioInfo::::::', audioInfo);
 
   useEffect(() => {
-    if (a) {
-      a.pause();
-      a = null;
+    if (audioFile) {
+      audioFile.pause();
+      audioFile = null;
       setButtonName('Play');
     }
     if (audio) {
-      a = new Audio(audio);
-      a.onended = () => {
+      audioFile = new Audio(audio);
+      audioFile.onended = () => {
         setButtonName('Play');
       };
     }
@@ -22,17 +26,23 @@ const AudioPlay = () => {
 
   const handleClick = () => {
     if (buttonName === 'Play') {
-      a.play();
+      audioFile.play();
       setButtonName('Pause');
     } else {
-      a.pause();
+      audioFile.pause();
       setButtonName('Play');
     }
   };
 
   const addFile = (e) => {
+    console.log('e.target.files[0]::::::', e.target.files[0]);
     if (e.target.files[0]) {
       setAudio(URL.createObjectURL(e.target.files[0]));
+
+      setAudioInfo({
+        name: e.target.files[0].name,
+        url: URL.createObjectURL(e.target.files[0]),
+      });
     }
   };
 
@@ -40,6 +50,11 @@ const AudioPlay = () => {
     <div>
       <button onClick={handleClick}>{buttonName}</button>
       <input type='file' onChange={addFile} />
+
+      <div>
+        <span>{audioInfo?.name}</span>
+        <audio controls src={audioInfo?.url} />
+      </div>
     </div>
   );
 };
